7a240ca62349c53bc92f547b9e8fceca7edfa3d6
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-06-27 13:03:10 +0530

    Update logomaker.py

--- Tianabot/modules/logomaker.py
+++ Tianabot/modules/logomaker.py
@@ -1,89 +1,80 @@
-from Tianabot.events import register
-from Tianabot import OWNER_ID
-from Tianabot import telethn as tbot
-import os 
-from PIL import Image, ImageDraw, ImageFont
+from bs4 import *
+import shutil
+import requests
+import os
+import base64
+import sys
+import random
+import requests
+from pyrogram import filters
+from Tianabot.services.pyrogram import pbot
 
 
-@register(pattern="^/logo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
- 
-  if not quew:
-     await event.reply('Darling Listen! Provide Some Text To Draw!')
-     return
- await event.reply('Tiana! Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./Tianabot/resources/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./Tianabot/resources/Maghrib.ttf", 330)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogobyTiana.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By Cutiepii_Robot")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report @PrinceBotSupport, {e}')
+def download_images(images): 
+    count = 0
+    print(f"Total {len(images)} Image Found!") 
+    if len(images) != 0:
+        for i, image in enumerate(images):
+            try:
+                image_link = image["data-srcset"] 
+            except: 
+                try: 
+                    image_link = image["data-src"] 
+                except:
+                    try:
+                        image_link = image["data-fallback-src"] 
+                    except:
+                        try:
+                            image_link = image["src"] 
+                        except: 
+
+                            pass
+            try: 
+                r = requests.get(image_link).content 
+                try:
+
+                    r = str(r, 'utf-8')
+                except UnicodeDecodeError:
+                    with open("logo@FridayOT.jpg", "wb+") as f: 
+                        f.write(r)
+                    count += 1
+            except: 
+                pass
+
+
+def mainne(name, typeo):
+    url = f"https://www.brandcrowd.com/maker/logos?text={name}&searchtext={typeo}&searchService="
+    r = requests.get(url) 
+    soup = BeautifulSoup(r.text, 'html.parser') 
+    images = soup.findAll('img') 
+    random.shuffle(images)
+    if images is not None:
+       print("level 1 pass")
+    download_images(images)
 
 
 
-   
-@register(pattern="^/logo1 ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
- 
-  if not quew:
-     await event.reply('Darling Listen! Provide Some Text To Draw!')
-     return
- await event.reply('Tiana! Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./Tianabot/resources/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./Tianabot/resources/Chopsic.otf", 1000)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogobyTiana.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By Cutiepii_Robot")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report @PrinceBotSupport, {e}')
-
-file_help = os.path.basename(__file__)
-file_help = file_help.replace(".py", "")
-file_helpo = file_help.replace("_", " ")
-
-__help__ = """
- • `/logo text`*:*  Create your logo with your name
- • `/logo1`*:*  Create your logo with your name
- """
-__mod_name__ = "Logo"
+@pbot.on_message(filters.command("logo") & ~filters.edited & ~filters.bot)
+async def logogen(client, message):
+    pablo = await client.send_message(message.chat.id,"`Creating The Logo.....`")
+    Godzilla = message.text
+    if not Godzilla:
+        await pablo.edit("Invalid Command Syntax, Please Check Help Menu To Know More!")
+        return
+    lmao = Godzilla.split(":", 1)
+    try:
+        typeo = lmao[1]
+    except BaseException:
+        typeo = "name"
+        await pablo.edit(
+             "Give name and type for logo Idiot. like `/logogen Daisy:Robot`")
+    name = lmao[0]
+    mainne(name, typeo)
+    caption = "<b>Logo Generated By @DaisyXBot. Powered by world's best userbot >> @FRIDAYOT <b>"
+    pate = "logo@DaisyXBOT.jpg"
+    await client.send_photo(message.chat.id, pate)
+    try:
+        os.remove(pate)
+    except:
+        pass
+    await pablo.delete()
