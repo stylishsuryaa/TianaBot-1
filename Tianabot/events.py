2199fdd60fca0b566f88c14a73a1de463e912c1d
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-06-28 21:01:01 +0530

    Update events.py

--- Tianabot/events.py
+++ Tianabot/events.py
@@ -198,7 +198,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.register = register
-        mod.tianabot = tianabot
+        mod.Tianabot = Tianabot
         mod.tbot = telethn
         mod.logger = logging.getLogger(shortname)
         spec.loader.exec_module(mod)
