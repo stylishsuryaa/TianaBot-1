f1e93e7134ccfa1649ba130070f6228c7161a323
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-06-28 14:27:11 +0530

    Update pyrogram.py

--- Tianabot/services/pyrogram.py
+++ Tianabot/services/pyrogram.py
@@ -19,7 +19,7 @@ from pyrogram import Client, errors
 from Tianabot.config import get_str_key, get_int_key
 
 TOKEN = get_str_key("TOKEN", required=True)
-APP_ID = get_int_key("APP_ID", required=True)
+API_ID = get_int_key("API_ID", required=True)
 APP_HASH = get_str_key("APP_HASH", required=True)
 session_name = TOKEN.split(':')[0]
 pbot = Client(session_name, api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
