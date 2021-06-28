bc50774bcf85c366f53ef03e873ee6dfb3e6a48f
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-06-28 12:40:16 +0530

    Update _pyrosongs.py

--- Tianabot/modules/_pyrosongs.py
+++ Tianabot/modules/_pyrosongs.py
@@ -57,8 +57,8 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @Rikudo_senin_bot'
-        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @TIANA_PRINCE_BOT'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')pt
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
@@ -75,4 +75,3 @@ def song(client, message):
         print(e)
 
 
-__mod_name__ = "pyrosong"
