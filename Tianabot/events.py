c57fe3c97d52a785cdf31e6b5aca57f997d23b72
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-06-28 12:03:25 +0530

    Update events.py

--- Tianabot/events.py
+++ Tianabot/events.py
@@ -18,21 +18,20 @@ gbanned = db.gban
 
 def register(**args):
     """ Registers a new message. """
-    pattern = args.get("pattern", None)
+    pattern = args.get('pattern', None)
 
-    r_pattern = r"^[/!]"
+    r_pattern = r'^[/!]'
 
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
 
-    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    args['pattern'] = pattern.replace('^/', r_pattern, 1)
 
     def decorator(func):
         telethn.add_event_handler(func, events.NewMessage(**args))
         return func
 
     return decorator
-
 
 def chataction(**args):
     """ Registers chat actions. """
