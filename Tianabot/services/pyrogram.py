94b7ddf5020612187a24746f8eed3645c90ff283
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-06-28 14:31:52 +0530

    Update pyrogram.py

--- Tianabot/services/pyrogram.py
+++ Tianabot/services/pyrogram.py
@@ -22,7 +22,7 @@ TOKEN = get_str_key("TOKEN", required=True)
 API_ID = get_int_key("API_ID", required=True)
 API_HASH = get_str_key("API_HASH", required=True)
 session_name = TOKEN.split(':')[0]
-pbot = Client(session_name, api_id=APP_ID, api_hash=APP_HASH, bot_token=TOKEN)
+pbot = Client(session_name, api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 # disable logging for pyrogram [not for ERROR logging]
 logging.getLogger('pyrogram').setLevel(level=logging.ERROR)
