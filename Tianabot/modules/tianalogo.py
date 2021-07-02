92a1082db30583d916cbe0471e8a85f46641e8c2
DEVIL-X <72757203+TEAMOFDEVIL-X@users.noreply.github.com>
2021-07-02 11:22:49 +0530

    Update tianalogo.py

--- Tianabot/modules/tianalogo.py
+++ Tianabot/modules/tianalogo.py
@@ -11,7 +11,7 @@ from Tianabot.events import register
 
 @register(pattern="^/logo ?(.*)")
 async def logo_gen(event):
-    xx = await eor(event, get_string("com_1"))
+    xx = await logo_gen(event, get_string("com_1"))
     name = event.pattern_match.group(1)
     if not name:
         await eod(xx, "`Give a name too!`")
